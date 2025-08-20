7eb6493cdb7e2de5f1bce1882d1bc421caf74f65
Dongyu Zhao <dy.zhao@outlook.com>
2025-08-20 11:07:05 +0800

    Add MarkdownContentBuilder

--- Sources/CodeParserCollection/Markdown/Nodes/MarkdownUnorderedListNodeBuilder.swift
+++ Sources/CodeParserCollection/Markdown/Nodes/MarkdownUnorderedListNodeBuilder.swift
@@ -8,7 +8,11 @@ public struct MarkdownUnorderedListNodeBuilder: CodeNodeBuilder {
   public typealias Node = MarkdownNodeElement
   public typealias Token = MarkdownTokenElement
 
-  public init() {}
+  private let contentBuilder: MarkdownContentBuilder
+
+  public init(contentBuilder: MarkdownContentBuilder = MarkdownContentBuilder()) {
+    self.contentBuilder = contentBuilder
+  }
 
   public func build(from context: inout CodeConstructContext<Node, Token>) -> Bool {
     let start = context.consuming
@@ -49,6 +53,12 @@ public struct MarkdownUnorderedListNodeBuilder: CodeNodeBuilder {
       let endTok = context.tokens[contentEnd - 1] as! MarkdownToken
       let range = startTok.range.lowerBound..<endTok.range.upperBound
       let para = ParagraphNode(range: range)
+
+      let inlineTokens = Array(context.tokens[contentStart..<contentEnd])
+      var inlineContext = CodeConstructContext(current: para, tokens: inlineTokens, state: context.state)
+      _ = contentBuilder.build(from: &inlineContext)
+      context.errors.append(contentsOf: inlineContext.errors)
+
       item.append(para)
     }
     list.append(item)
